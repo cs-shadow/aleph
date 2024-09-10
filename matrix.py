6f85857c7fb73b5213f8670e19228707e107928e
Chandan Singh <chandan@chandansingh.net>
2024-09-10 18:52:50 +0100

    Fill out stub code

--- matrix.py
+++ matrix.py
@@ -1,7 +1,13 @@
+# TODO: Implement this
 def multiply(matrix_a, matrix_b):
     return result
 
+# Generate two matrices based on random numbers
+# HINT: you can use something like `random.randint(1, 1000)`
+# TODO: Implement this
 matrix_a = ...
 matrix_b = ...
 
-multiply(matrix_a, matrix_b)
+# Multiply the matrices and print the result
+result = multiply(matrix_a, matrix_b)
+# TODO: Write code to print the result on screen
