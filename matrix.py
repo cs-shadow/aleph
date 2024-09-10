75e16455ff29204d4cf78c276f190c08c12286a9
Chandan Singh <chandan@chandansingh.net>
2024-09-10 20:37:23 +0100

    Replace matrix example with hello

--- matrix.py
+++ matrix.py
@@ -1,13 +1,1 @@
-# TODO: Implement this
-def multiply(matrix_a, matrix_b):
-    return result
-
-# Generate two matrices based on random numbers
-# HINT: you can use something like `random.randint(1, 1000)`
-# TODO: Implement this
-matrix_a = ...
-matrix_b = ...
-
-# Multiply the matrices and print the result
-result = multiply(matrix_a, matrix_b)
-# TODO: Write code to print the result on screen
+hello
