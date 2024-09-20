c26e661d5c88bea1caa1cb7c4073c28fc87b2b03
Boshra Besharatian <boshrabesharatian@Boshras-Laptop.local>
2024-09-20 10:58:27 -0500

    matrix multiplication

--- matrix.py
+++ matrix.py
@@ -1,13 +1,7 @@
-# TODO: Implement this
-def multiply(matrix_a, matrix_b):
-    return result
+### Developed by Boshra
+import numpy as np
 
-# Generate two matrices based on random numbers
-# HINT: you can use something like `random.randint(1, 1000)`
-# TODO: Implement this
-matrix_a = ...
-matrix_b = ...
+A = np.random.uniform(low=0, high=1000, size=(20, 20))
+B = np.random.uniform(low=0, high=1000, size=(20, 20))
 
-# Multiply the matrices and print the result
-result = multiply(matrix_a, matrix_b)
-# TODO: Write code to print the result on screen
+C = np.dot(A, B)
